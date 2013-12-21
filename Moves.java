818df398043fec9a2bf9a58b1f736fde9db07e0b
Valtis <erkka_kaaria@outlook.com>
2013-12-21 18:31:32 +0200

    Fleshing out the statebot a bit.

--- Moves.java
+++ Moves.java
@@ -1,12 +1,10 @@
-/**
- * Created by Omistaja on 21.12.2013.
- */
+
 public enum Moves {
     WAIT,
     MOVE_UP,
     MOVE_DOWN,
     MOVE_LEFT,
     MOVE_RIGHT,
-    BOMB;
+    BOMB
 
 }
--- StateMachineBot.java
+++ StateMachineBot.java
@@ -1,11 +1,45 @@
 import java.util.List;
 
 public class StateMachineBot extends Bot {
+    private enum State {
+        SEEK,
+        ESCAPE
+    }
 
+    private State currentState;
+    String currentCommand;
+
+
+    public StateMachineBot(){
+        currentState = State.SEEK;
+    }
 
     @Override
     public String GetCommand(List<String> lines) {
+        currentCommand = GetCommandString(Moves.WAIT);
+        UpdateState();
+        return currentCommand;
+    }
 
-        return null;
+    private void UpdateState() {
+        switch (currentState) {
+            case SEEK:
+                Seek();
+                break;
+            case ESCAPE:
+                Escape();
+                break;
+            default:
+                throw new RuntimeException("Shouldn't happen...");
+        }
+    }
+
+    private void Seek() {
+
+    }
+
+    private void Escape() {
+
+
     }
 }
